c99289eabfbc707e77eced5624efb0bfd53f4d76
ishika-2012 <19bcs1564@gmail.com>
2023-09-21 10:26:09 +0530

    first commit

---
+++ src/components/Video.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { Player } from "react-tuby";
+import "react-tuby/css/main.css";
+function Video() {
+  return (
+    <div style={{ position: 'relative', height: "50%", width: "100%" }}>
+
+      <Player
+        
+        src={[
+          {
+            quality: "Full HD",
+            url:
+              "https://cdn.glitch.me/cbf2cfb4-aa52-4a1f-a73c-461eef3d38e8/1080.mp4",
+          },
+          {
+            quality: 720,
+            url: "https://cdn.glitch.me/cbf2cfb4-aa52-4a1f-a73c-461eef3d38e8/720.mp4",
+          },
+          {
+            quality: 480,
+            url: "https://cdn.glitch.me/cbf2cfb4-aa52-4a1f-a73c-461eef3d38e8/480.mp4",
+          },
+        ]}
+        subtitles={[
+          {
+            lang: "en",
+            language: "English",
+            url:
+              "https://cdn.jsdelivr.net/gh/naptestdev/video-examples@master/en.vtt",
+          },
+
+        ]}
+
+        poster="https://cdn.jsdelivr.net/gh/naptestdev/video-examples@master/poster.png"
+      />
+    </div>
+
+      )
+
+}
+
+      export default Video;
